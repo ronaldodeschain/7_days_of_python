50086a80fa7269c840f8835253fb944001b37d5b
Ronaldo Lemos <lotushall@gmail.com>
2025-12-22 14:49:38 -0300

    adicionando paginação e estilo aos botões do nav

--- pythonchallenge/app/views.py
+++ pythonchallenge/app/views.py
@@ -2,17 +2,18 @@ import httpx
 from django.shortcuts import render
 from googletrans import Translator
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 async def listar_personagens(request):
-    api_url = "https://last-airbender-api.fly.dev/api/v1/characters"
+    url = "https://last-airbender-api.fly.dev/api/v1/characters"
     translator = Translator()
     personagens_traduzidos = []
 
     # Use um cliente HTTP assíncrono
     async with httpx.AsyncClient() as client:
-        response = await client.get(api_url)
+        response = await client.get(url)
         personagens = response.json()
-        
+
         for personagem in personagens:
             nome_original = personagem.get('name', '')
             afiliacao_original = personagem.get('affiliation', 'Nenhuma')
@@ -25,6 +26,23 @@ async def listar_personagens(request):
             
             personagem['nome_traduzido'] = nome_traduzido_obj.text
             personagem['afiliacao_traduzida'] = afiliacao_traduzida_obj.text
-            personagem['photoUrl'] = personagem.get('photoUrl', '')
+
     context = {'personagens': personagens}
+
+    # Pagination
+    paginator = Paginator(personagens, 10) # Show 10 characters per page
+    page = request.GET.get('page')
+    try:
+        personagens = paginator.page(page)
+    except PageNotAnInteger:
+    
+        personagens = paginator.page(1)
+    except EmptyPage:
+    
+        personagens = paginator.page(paginator.num_pages)
+
+    context = {
+    'personagens': personagens,
+    'page': page
+    }
     return render(request, 'app/personagens.html', context)
